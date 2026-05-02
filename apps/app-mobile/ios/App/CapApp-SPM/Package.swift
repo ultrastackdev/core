cbfe374e3421fcbd242e4e1ea7e574e1782649d0
Youssef Hamdi <youssef7amdi@gmail.com>
2026-05-02 15:28:34 +0300

    chore: upgrade to latest nx version

--- apps/app-mobile/ios/App/CapApp-SPM/Package.swift
+++ apps/app-mobile/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.1"),
         .package(name: "CapacitorStatusBar", path: "..\..\..\..\..\node_modules\@capacitor\status-bar")
     ],
     targets: [
